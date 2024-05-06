4059f2bfd9ab895dffe4dbe7e8e4e220f4e48816
AsutoshAcharya <asutosh246@gmail.com>
2024-05-06 22:39:07 +0530

    added persist store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { Task, TaskStatus } from "./types";
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 
 export type StoreState = {
   tasks: Array<Task>;
@@ -10,24 +10,34 @@ export type StoreState = {
   moveTask: (id: string, status: TaskStatus) => void;
 };
 
-const useTaskStore = create<StoreState, [["zustand/devtools", StoreState]]>(
-  devtools((set, get) => ({
-    tasks: [{ title: "learn zustand", status: "planned", id: "2.3232" }],
-    getSpecificTasks: (status) =>
-      get().tasks.filter((task) => task.status === status),
-    addTask: (task) => {
-      set((store) => ({ tasks: [...store.tasks, task] }));
-    },
-    deleteTask: (id) =>
-      set((store) => ({ tasks: store.tasks.filter((task) => task.id !== id) })),
-    moveTask: (id, status) =>
-      set((store) => ({
-        tasks: store.tasks.map((task) => {
-          if (task.id !== id) return task;
-          return { ...task, status };
-        }),
-      })),
-  }))
+const useTaskStore = create<
+  StoreState,
+  [["zustand/devtools", StoreState], ["zustand/persist", StoreState]]
+>(
+  devtools(
+    persist(
+      (set, get) => ({
+        tasks: [{ title: "learn zustand", status: "planned", id: "2.3232" }],
+        getSpecificTasks: (status) =>
+          get().tasks.filter((task) => task.status === status),
+        addTask: (task) => {
+          set((store) => ({ tasks: [...store.tasks, task] }));
+        },
+        deleteTask: (id) =>
+          set((store) => ({
+            tasks: store.tasks.filter((task) => task.id !== id),
+          })),
+        moveTask: (id, status) =>
+          set((store) => ({
+            tasks: store.tasks.map((task) => {
+              if (task.id !== id) return task;
+              return { ...task, status };
+            }),
+          })),
+      }),
+      { name: "Task-Manager" }
+    )
+  )
 );
 
 export { useTaskStore };
