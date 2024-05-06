1810ed1aa19174b7fd6fd3b5c394efbd608231ff
AsutoshAcharya <asutosh246@gmail.com>
2024-05-06 21:17:15 +0530

    added set on store

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,50 @@
 //local imports
+import { useState } from "react";
 import "./App.css";
 import "./index.css";
 import Column from "./screens/Home/Column";
 import { TaskStatus } from "./store/types";
+import { useTaskStore } from "./store/store";
 
 const states = ["planned", "ongoing", "done"];
 
 function App() {
+  const [task, setTask] = useState<string>("");
+  const addTaskToStore = useTaskStore((store) => store.addTask);
+
   return (
-    <div className="flex content-center justify-center w-screen h-screen font-sans bg-slate-500">
-      {states.map((state) => (
-        <Column key={state} state={state as TaskStatus} />
-      ))}
+    <div className="flex flex-col w-screen h-screen font-sans bg-slate-500">
+      <form
+        className="flex content-center justify-center w-full gap-2 p-2"
+        onSubmit={(e) => {
+          e.preventDefault();
+          addTaskToStore({
+            id: (Math.random() * 10000).toString(),
+            title: task,
+            status: "planned",
+          });
+          setTask("");
+        }}
+      >
+        <input
+          type="text"
+          className="w-1/3 rounded-md"
+          value={task}
+          required
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button
+          disabled={task.length === 0}
+          className="p-2 text-white bg-purple-500 rounded-md cursor-pointer hover:bg-purple-300 disabled:bg-gray-600 disabled:cursor-not-allowed"
+        >
+          Add Task
+        </button>
+      </form>
+      <div className="flex content-center justify-center w-full h-full">
+        {states.map((state) => (
+          <Column key={state} state={state as TaskStatus} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -5,13 +5,17 @@ import { devtools } from "zustand/middleware";
 export type StoreState = {
   tasks: Array<Task>;
   getSpecificTasks: (status: TaskStatus) => Array<Task>;
+  addTask: (task: Task) => void;
 };
 
 const useTaskStore = create<StoreState, [["zustand/devtools", StoreState]]>(
   devtools((set, get) => ({
-    tasks: [{ title: "learn zustand", status: "planned" }],
+    tasks: [{ title: "learn zustand", status: "planned", id: "2.3232" }],
     getSpecificTasks: (status) =>
       get().tasks.filter((task) => task.status === status),
+    addTask: (task) => {
+      set((store) => ({ tasks: [...store.tasks, task] }));
+    },
   }))
 );
 
